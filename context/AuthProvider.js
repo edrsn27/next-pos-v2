537949b99ad666c902197d107df605d2bc8e24db
ederson <edersonvillegas50@gmail.com>
2022-06-05 06:44:42 +0800

    initial context

---
+++ context/AuthProvider.js
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+import { auth } from "../../firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const signin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signout = () => {
+    auth.signOut();
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = { currentUser, loading, signin, signout };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
